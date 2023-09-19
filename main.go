b11027d56b3a2ce15da17b29d34c572665cde309
Leonan <pedroleonan85@gmail.com>
2023-09-19 08:04:50 -0300

    Testes

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,17 +10,5 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world")
 	})
-
-	app.Get("/env", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, ENV! " + os.Getenv("TEST_ENV"))
-	})
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "3000"
-	}
-
-	log.Fatal(app.Listen("0.0.0.0:" + port))
 
 }
